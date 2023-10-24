365d0222047d0ab1db0798c5cfef8e81855667b8
jorzel <jaroslaw.orzel@saucelabs.com>
2023-10-24 15:42:54 +0200

    Simple traces for python app

--- user-service/app.py
+++ user-service/app.py
@@ -11,10 +11,12 @@ tracer = trace.get_tracer("my.tracer.name")
 
 @app.route("/users/<string:username>/limits")
 def reservation_limits(username):
-    record = _fetch_record(username)
-    return jsonify(record)
+    with tracer.start_as_current_span("reservation_limits"):
+        record = _fetch_record(username)
+        return jsonify(record)
 
 
+@tracer.start_as_current_span("fetch_record")
 def _fetch_record(username):
     time.sleep(randint(0, 3))
     return {
