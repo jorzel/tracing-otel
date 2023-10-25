f59ff7a44215e6b5ebbc9846fa5f0d7125a53b79
jorzel <jaroslaw.orzel@saucelabs.com>
2023-10-25 19:48:48 +0200

    Add otel exporter and collector to python project

--- user-service/app.py
+++ user-service/app.py
@@ -3,22 +3,44 @@ from random import randint
 
 from flask import Flask, jsonify
 from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+resource = Resource(
+    attributes={
+        "service.name": "user-service",
+    }
+)
+
+provider = TracerProvider()
+otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
+processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(processor)
+
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer("user-service")
 
 app = Flask(__name__)
-
-tracer = trace.get_tracer("my.tracer.name")
 
 
 @app.route("/users/<string:username>/limits")
 def reservation_limits(username):
     with tracer.start_as_current_span("reservation_limits"):
+        _validate_username(username)
         record = _fetch_record(username)
         return jsonify(record)
 
 
+@tracer.start_as_current_span("validate_username")
+def _validate_username(username):
+    time.sleep(0.1 * randint(0, 10))
+
+
 @tracer.start_as_current_span("fetch_record")
 def _fetch_record(username):
-    time.sleep(randint(0, 3))
+    time.sleep(0.1 * randint(0, 30))
     return {
         "username": username,
         "limits": {
